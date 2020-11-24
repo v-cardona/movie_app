d49a5970b931113a73f5e687197f41edfbff58fa
Victor Cardona <vicardona7@hotmail.com>
2020-11-24 19:43:59 +0100

    added api tmdb key

--- lib/data/core/api_constants.dart
+++
@@ -1,7 +0,0 @@
-class ApiConstants { 
-  ApiConstants._();
-  
-  static const String BASE_URL = "https://api.themoviedb.org/3/";
-  static const String API_KEY = "YOUR API KEY";
-  static const String BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-}
