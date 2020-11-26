09ae4b16f722af8608ab915c3a27d114582e6207
Victor Cardona <vicardona7@hotmail.com>
2020-11-26 19:51:32 +0100

    created ui for bottom part of homescreen, horizontal list view of each fetch movies

--- lib/presentation/journeys/home/home_screen.dart
+++ lib/presentation/journeys/home/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:movie_app/presentation/blocs/movie_backdrop/movie_backdrop_bloc.
 import 'package:movie_app/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
 import 'package:movie_app/presentation/blocs/movie_tabbed/movie_tabbed_bloc.dart';
 import 'package:movie_app/presentation/journeys/home/movie_carousel/movie_carousel_widget.dart';
+import 'package:movie_app/presentation/journeys/home/movie_tabbed/movie_tabbed_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -63,7 +64,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   FractionallySizedBox(
                     alignment: Alignment.bottomCenter,
                     heightFactor: 0.4,
-                    child: Placeholder(color: Colors.white),
+                    child: MovieTabbedWidget(),
                   ),
                 ],
               );
---
+++ lib/presentation/journeys/home/movie_tabbed/tab.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Tab {
+
+  final int index;
+  final String title;
+
+  const Tab({
+    @required this.index,
+    @required this.title,
+  })  : assert(index >= 0, 'index cannot be negative'),
+        assert(title != null, 'title cannot be null');
+}
