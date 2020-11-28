75510c16edacc8fe684109018e812b394923262b
Victor Cardona <vicardona7@hotmail.com>
2020-11-28 11:39:30 +0100

    added language bloc

--- lib/di/get_it.dart
+++ lib/di/get_it.dart
@@ -9,6 +9,7 @@ import 'package:movie_app/domain/usecases/get_coming_soon.dart';
 import 'package:movie_app/domain/usecases/get_playing_now.dart';
 import 'package:movie_app/domain/usecases/get_popular.dart';
 import 'package:movie_app/domain/usecases/get_trending.dart';
+import 'package:movie_app/presentation/blocs/language/language_bloc.dart';
 import 'package:movie_app/presentation/blocs/movie_backdrop/movie_backdrop_bloc.dart';
 import 'package:movie_app/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
 import 'package:movie_app/presentation/blocs/movie_tabbed/movie_tabbed_bloc.dart';
@@ -38,9 +39,10 @@ Future init() async {
     movieBackdropBloc: getItInstance()));
   getItInstance.registerLazySingleton(() => MovieBackdropBloc());
   getItInstance.registerFactory(() => MovieTabbedBloc(
-    getPopular: GetPopular(getItInstance()),
-    getComingSoon: GetComingSoon(getItInstance()),
-    getPlayingNow: GetPlayingNow(getItInstance()),
-  ),
-);
+      getPopular: GetPopular(getItInstance()),
+      getComingSoon: GetComingSoon(getItInstance()),
+      getPlayingNow: GetPlayingNow(getItInstance()),
+    ),
+  );
+  getItInstance.registerSingleton<LanguageBloc>(LanguageBloc());
 }
