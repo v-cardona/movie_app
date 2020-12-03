2ee13d88ab55d7816b6fad65ab4069df7e7fe185
Victor Cardona <vicardona7@hotmail.com>
2020-12-03 18:25:55 +0100

    general ui for movie detail

--- lib/presentation/journeys/movie_detail/movie_detail_screen.dart
+++ lib/presentation/journeys/movie_detail/movie_detail_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:movie_app/common/constants/size_constants.dart';
+import 'package:movie_app/common/extensions/size_extensions.dart';
+import 'package:movie_app/di/get_it.dart';
+import 'package:movie_app/presentation/blocs/movie_detail/movie_detail_bloc.dart';
 import 'package:movie_app/presentation/journeys/movie_detail/movie_detail_arguments.dart';
 
-class MovieDetailScreen extends StatelessWidget {
+import 'big_poster.dart';
+
+class MovieDetailScreen extends StatefulWidget {
 
   final MovieDetailArguments movieDetailArguments;
 
@@ -12,8 +20,64 @@ class MovieDetailScreen extends StatelessWidget {
       super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
+  _MovieDetailScreenState createState() => _MovieDetailScreenState();
+}
+
+class _MovieDetailScreenState extends State<MovieDetailScreen> {
+  
+  MovieDetailBloc _movieDetailBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _movieDetailBloc = getItInstance<MovieDetailBloc>();
+    _movieDetailBloc.add(
+      MovieDetailLoadEvent(
+        widget.movieDetailArguments.movieId,
+      ),
     );
   }
+  
+  @override
+  void dispose() {
+    _movieDetailBloc?.close();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocProvider<MovieDetailBloc>.value(
+        value: _movieDetailBloc,
+        child: BlocBuilder<MovieDetailBloc, MovieDetailState>(
+          builder: (context, state) {
+            if (state is MovieDetailLoaded) {
+              final movieDetail = state.movieDetailEntity;
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  BigPoster(
+                    movie: movieDetail
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: Sizes.dimen_16.w
+                    ),
+                    child: Text(
+                      movieDetail.overview,
+                      style: Theme.of(context).textTheme.bodyText2,
+                    ),
+                  )
+                ],
+              );
+            } else if (state is MovieDetailError) {
+              return Container();
+            }
+            return SizedBox.shrink();
+          },
+        ),
+      ),
+    );
+  }
+
 }
